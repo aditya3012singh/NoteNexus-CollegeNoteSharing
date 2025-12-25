57ea862a735611111070a654c05575cdd009eea4
Aditya <aditya.2428cseai@gmail.com>
2025-12-25 06:12:33 +0530

    cors setted

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(
   cors(
     {
-      origin: "https://notesnexuskiet.netlify.app/",
+      origin: "https://notesnexuskiet.netlify.app",
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     }
